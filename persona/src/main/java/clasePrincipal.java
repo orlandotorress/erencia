fb707160736292a33bdd3c6680c952fbfe9da87b
orlandotorress <orlytorres60@gmail.com>
2024-09-01 16:04:01 -0500

    Update clasePrincipal.java

--- persona/src/main/java/clasePrincipal.java
+++ persona/src/main/java/clasePrincipal.java
@@ -4,7 +4,7 @@ public class clasePrincipal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Solicitar datos al usuario
+        
         System.out.print("Ingrese el nombre: ");
         String nombre = scanner.nextLine();
 
@@ -20,7 +20,7 @@ public class clasePrincipal {
         System.out.print("Ingrese la edad: ");
         int edad = scanner.nextInt();
 
-        scanner.nextLine(); // Consumir el salto de línea
+        scanner.nextLine(); 
 
         System.out.print("Ingrese el email: ");
         String email = scanner.nextLine();
@@ -28,7 +28,7 @@ public class clasePrincipal {
         System.out.print("Ingrese el salario: ");
         double salario = scanner.nextDouble();
 
-        scanner.nextLine(); // Consumir el salto de línea
+        scanner.nextLine(); 
 
         System.out.print("Ingrese el nombre del jefe inmediato: ");
         String jefeInmediato = scanner.nextLine();
@@ -36,10 +36,10 @@ public class clasePrincipal {
         System.out.print("Ingrese los años de experiencia: ");
         int añosExperiencia = scanner.nextInt();
 
-        // Crear el objeto Empleado
+
         hija empleado = new hija (nombre, apellido, direccion, telefono, edad, email, salario, jefeInmediato, añosExperiencia);
 
-        // Imprimir el detalle del empleado
+        
         empleado.imprimirDetalleEmpleado();
 
         scanner.close();
